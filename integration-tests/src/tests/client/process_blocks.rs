e175bb3840b5bbe2e383d0a7e49600988a5045eb
Longarithm <the.aleksandr.logunov@gmail.com>
2022-02-26 03:13:36 +0300

    comp fix

--- integration-tests/src/tests/client/process_blocks.rs
+++ integration-tests/src/tests/client/process_blocks.rs
@@ -17,7 +17,7 @@ use near_chain::{
     Block, ChainGenesis, ChainStore, ChainStoreAccess, ChainStoreUpdate, ErrorKind, Provenance,
     RuntimeAdapter,
 };
-use near_chain_configs::{ClientConfig, Genesis};
+use near_chain_configs::{ClientConfig, Genesis, GenesisValidationMode};
 use near_chunks::{ChunkStatus, ShardsManager};
 use near_client::test_utils::{
     create_chunk_on_height, run_catchup, setup_client, setup_mock, setup_mock_all_validators,
@@ -66,7 +66,7 @@ use near_store::db::DBCol::ColStateParts;
 use near_store::test_utils::create_test_store;
 use near_store::{create_store, get, HEAD_KEY};
 use nearcore::config::{GenesisExt, TESTING_INIT_BALANCE, TESTING_INIT_STAKE};
-use nearcore::{get_store_path, TrackedConfig, NEAR_BASE};
+use nearcore::{get_store_path, load_config, TrackedConfig, NEAR_BASE};
 use rand::prelude::StdRng;
 use rand::{Rng, SeedableRng};
 
@@ -4517,8 +4517,9 @@ mod contract_precompilation_tests {
 fn test_process_blocks() {
     // init_test_logger();
     let home_dir = Path::new("/home/ubuntu/.near");
+    let near_config = load_config(&home_dir, GenesisValidationMode::UnsafeFast);
     let store = create_store(&get_store_path(&home_dir));
-    let height = 55144899; //55145899; //9820210; //near_config.genesis.config.genesis_height;
+    let height = near_config.genesis.config.genesis_height; //55144899; //55145899; //9820210; //
 
     let mut chain_store = ChainStore::new(store.clone(), height);
     let block_hash = chain_store.get_block_hash_by_height(height);
