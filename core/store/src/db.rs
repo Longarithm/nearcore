ddac469aa57a5623bd1cfc08578f362aa9cc72f7
Longarithm <the.aleksandr.logunov@gmail.com>
2022-04-30 04:31:03 +0400

    minor fix

--- core/store/src/db.rs
+++ core/store/src/db.rs
@@ -529,6 +529,7 @@ fn rocksdb_block_based_options(block_size: usize, cache_size: usize) -> BlockBas
 fn choose_cache_size(col: DBCol, store_config: &StoreConfig) -> usize {
     match col {
         DBCol::State => store_config.col_state_cache_size,
+        DBCol::ColFlatState => store_config.col_state_cache_size,
         _ => 32 * 1024 * 1024,
     }
 }
