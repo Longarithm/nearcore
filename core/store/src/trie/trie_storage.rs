fb1130ebd8d052b83a5ec94cb33b64d01a61f371
Longarithm <the.aleksandr.logunov@gmail.com>
2022-04-30 04:15:58 +0400

    minor fix

--- core/store/src/trie/trie_storage.rs
+++ core/store/src/trie/trie_storage.rs
@@ -232,6 +232,8 @@ impl TrieCachingStorage {
 
 impl TrieStorage for TrieCachingStorage {
     fn retrieve_raw_bytes(&self, hash: &CryptoHash) -> Result<Arc<[u8]>, StorageError> {
+        let _span = tracing::debug_span!(target: "runtime", "retrieve_raw_bytes").entered();
+
         // Try to get value from chunk cache containing nodes with cheaper access. We can do it for any `TrieCacheMode`,
         // because we charge for reading nodes only when `CachingChunk` mode is enabled anyway.
         if let Some(val) = self.chunk_cache.borrow_mut().get(hash) {
