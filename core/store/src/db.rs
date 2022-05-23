b4398364a1fa6d636687fac3973183fafdeff4c6
Longarithm <the.aleksandr.logunov@gmail.com>
2022-05-23 19:17:44 +0400

    latency get

--- core/store/src/db.rs
+++ core/store/src/db.rs
@@ -549,7 +549,7 @@ fn rocksdb_block_based_options(block_size: usize, cache_size: usize) -> BlockBas
 
 fn choose_cache_size(col: DBCol, store_config: &StoreConfig) -> usize {
     match col {
-        DBCol::State => 1, //store_config.col_state_cache_size,
+        DBCol::State => store_config.col_state_cache_size,
         _ => 32 * 1024 * 1024,
     }
 }
