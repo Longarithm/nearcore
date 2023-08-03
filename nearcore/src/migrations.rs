f1d1619ee0fecfd59cf95b9aac3ad2e12b166db0
Longarithm <the.aleksandr.logunov@gmail.com>
2023-08-03 16:49:04 +0400

    fix

--- nearcore/src/migrations.rs
+++ nearcore/src/migrations.rs
@@ -112,6 +112,7 @@ impl<'a> near_store::StoreMigrator for Migrator<'a> {
                 Ok(())
             }
             36 => near_store::migrations::migrate_36_to_37(store),
+            37 => unreachable!(),
             DB_VERSION.. => unreachable!(),
         }
     }
