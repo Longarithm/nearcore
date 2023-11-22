3625c0ecb275ce936094218d36482a73d23a7b47
Longarithm <the.aleksandr.logunov@gmail.com>
2023-11-22 22:58:01 +0100

    debug

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -4142,7 +4142,7 @@ impl Chain {
 
                 return Ok(jobs);
             })
-            .collect::<Vec<_>>()
+            .collect::<Vec<Vec<_>>>()
         .into_iter()
         .flatten()
         .collect::<Vec<_>>()
