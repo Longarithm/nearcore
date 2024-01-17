d8148aa5776445d6f1381cb04d4cbf0c5d44eeb5
Longarithm <the.aleksandr.logunov@gmail.com>
2024-01-17 20:36:25 -0300

    debug

--- chain/client/src/chunk_validation.rs
+++ chain/client/src/chunk_validation.rs
@@ -345,6 +345,7 @@ fn validate_chunk_state_witness(
         .implicit_transition_params
         .into_iter()
         .zip(state_witness.implicit_transitions.into_iter())
+        .rev()
     {
         let block_hash = block.block_hash;
         let old_chunk_data = OldChunkData {
@@ -449,12 +450,12 @@ impl Client {
             )));
         }
 
-        let prev_blocks = self.chain.get_blocks_until_height(
+        let mut prev_blocks = self.chain.get_blocks_until_height(
             *chunk_header.prev_block_hash(),
             prev_chunk_height_included,
             true,
         )?;
-        // prev_blocks.reverse();
+        prev_blocks.reverse();
         let (main_block, implicit_blocks) = prev_blocks.split_first().unwrap();
         let store = self.chain.chain_store().store();
         let StoredChunkStateTransitionData { base_state, receipts_hash } = store
