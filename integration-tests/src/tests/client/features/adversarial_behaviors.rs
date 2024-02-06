9e0571495be16c4ec0c93570f56170f039fd73de
Longarithm <the.aleksandr.logunov@gmail.com>
2024-02-06 21:25:15 +0400

    debug

--- integration-tests/src/tests/client/features/adversarial_behaviors.rs
+++ integration-tests/src/tests/client/features/adversarial_behaviors.rs
@@ -8,7 +8,7 @@ use near_network::{
     shards_manager::ShardsManagerRequestFromNetwork,
     types::{NetworkRequests, PeerManagerMessageRequest},
 };
-use near_o11y::testonly::init_test_logger;
+use near_o11y::testonly::{init_integration_logger, init_test_logger};
 use near_primitives::{
     shard_layout::ShardLayout,
     types::{AccountId, EpochId, ShardId},
@@ -347,7 +347,7 @@ fn test_banning_chunk_producer_when_seeing_invalid_chunk_base(
 #[test]
 #[cfg(feature = "test_features")]
 fn test_banning_chunk_producer_when_seeing_invalid_chunk() {
-    init_test_logger();
+    init_integration_logger();
     let mut test = AdversarialBehaviorTestData::new();
     test.env.clients[7].produce_invalid_chunks = true;
     test_banning_chunk_producer_when_seeing_invalid_chunk_base(test);
@@ -356,7 +356,7 @@ fn test_banning_chunk_producer_when_seeing_invalid_chunk() {
 #[test]
 #[cfg(feature = "test_features")]
 fn test_banning_chunk_producer_when_seeing_invalid_tx_in_chunk() {
-    init_test_logger();
+    init_integration_logger();
     let mut test = AdversarialBehaviorTestData::new();
     test.env.clients[7].produce_invalid_tx_in_chunks = true;
     test_banning_chunk_producer_when_seeing_invalid_chunk_base(test);
