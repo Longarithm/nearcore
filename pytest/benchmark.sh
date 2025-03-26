3420376a7d6e2e3f5e417835928baf3594a7dc49
Longarithm <the.alex.logunov@gmail.com>
2025-03-26 17:43:04 +0500

    prefixes

--- pytest/benchmark.sh
+++ pytest/benchmark.sh
@@ -190,7 +190,7 @@ run_mode_1() {
             --rpc-url $rpc_url \
             --signer-key-path /home/ubuntu/.near/validator_key.json \
             $nonce_arg \
-            --sub-account-prefix '2,c,f,h,m,x' \
+            --sub-account-prefixes '2,c,f,h,m,x' \
             --num-sub-accounts 500 \
             --deposit 953060601875000000010000000 \
             --channel-buffer-size 1200 \
